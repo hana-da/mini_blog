1d56d7dc419545445f1392d3d2176f1fea39ea35
hana-da <hana-da@users.noreply.github.com>
2020-04-24 09:29:34 +0900

    Set MiniBlogController#root to root_path

--- spec/features/mini_blog_controller_spec.rb
+++ spec/features/mini_blog_controller_spec.rb
@@ -3,5 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe 'MiniBlogControllers', type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  describe 'root access' do
+    it do
+      visit root_path
+
+      expect(page).to have_http_status(:success)
+    end
+  end
 end
