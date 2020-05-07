1c4474a298a93a969c75127a0bddd026aee7f332
hana-da <hana-da@users.noreply.github.com>
2020-05-07 15:01:32 +0900

    Remove unused view helpers

--- spec/helpers/blogs_helper_spec.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-require 'rails_helper'
-
-RSpec.describe BlogsHelper, type: :helper do # rubocop:disable RSpec/EmptyExampleGroup
-end
--- spec/helpers/users_helper_spec.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-require 'rails_helper'
-
-RSpec.describe UsersHelper, type: :helper do # rubocop:disable RSpec/EmptyExampleGroup
-end
