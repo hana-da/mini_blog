93585aa78a900861a4295de2e63bc0de66596db4
hana-da <hana-da@users.noreply.github.com>
2020-05-07 15:00:53 +0900

    Suppress generation of view helpers

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -2,14 +2,14 @@
 
 Rails.application.config.generators do |g|
   g.assets false
-  g.helper true
+  g.helper false
   g.controller skip_routes: true
   g.test_framework :rspec,
                    request_specs:    false,
                    controller_specs: false,
                    view_specs:       false,
                    routing_specs:    false,
-                   helper_specs:     true,
+                   helper_specs:     false,
                    fixture:          true
   g.fixture_replacement :factory_bot, dir: 'spec/factories'
 end
