ad11e8fa915f24587a63f0508d1df143a9c8b1dd
hana-da <hana-da@users.noreply.github.com>
2020-05-08 11:44:29 +0900

    Remove unused devise/registrations routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
   resources :blogs, only: :create
 
+  devise_for :users, skip: :registration
+  devise_scope :user do
+    get  '/users/sign_up(.:format)', to: 'devise/registrations#new',    as: 'new_user_registration'
+    post '/users(.:format)',         to: 'devise/registrations#create', as: 'user_registration'
+  end
   get 'users/:username', to: 'users#show', as: :user
 
   root 'mini_blog#root'
