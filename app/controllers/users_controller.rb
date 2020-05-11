8df6ee197f08712b3ba8a4ba4f17d2b7bc88925c
hana-da <hana-da@users.noreply.github.com>
2020-05-11 17:23:12 +0900

    Follow and unfollow require authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :authenticate_user!, except: :show
+
   def show
     @user = User.find_by(username: params[:username]) || raise(ActiveRecord::RecordNotFound)
   end
---
+++ spec/requests/users_controllers_spec.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'UsersControllers', type: :request do
+  let!(:user)       { FactoryBot.create(:user) }
+  let!(:other_user) { FactoryBot.create(:user) }
+
+  context 'ログインしている時' do
+    before { sign_in user }
+
+    it 'フォローできる' do
+      expect(user.following).not_to include(other_user)
+
+      expect do
+        post follow_user_path, params: { id: other_user.id }
+      end.to change(UserRelationship, :count).by(1)
+
+      expect(response).to have_http_status(:found)
+      expect(response).to redirect_to(root_path)
+    end
+
+    it 'フォロー解除できる' do
+      user.follow(other_user)
+      expect(user.following).to include(other_user)
+
+      expect do
+        post unfollow_user_path, params: { id: other_user.id }
+      end.to change(UserRelationship, :count).by(-1)
+
+      expect(response).to have_http_status(:found)
+      expect(response).to redirect_to(root_path)
+    end
+  end
+
+  context 'ログインしていない時' do
+    it 'フォローできない' do
+      expect(user.following).not_to include(other_user)
+
+      expect do
+        post follow_user_path, params: { id: other_user.id }
+      end.not_to change(UserRelationship, :count)
+
+      expect(response).to have_http_status(:found)
+      expect(response).to redirect_to(new_user_session_path)
+    end
+
+    it 'フォロー解除できない' do
+      user.follow(other_user)
+      expect(user.following).to include(other_user)
+
+      expect do
+        post follow_user_path, params: { id: other_user.id }
+      end.not_to change(UserRelationship, :count)
+
+      expect(response).to have_http_status(:found)
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
+end
