3e4f9215e9eb110496a643cd51411628115f3576
hana-da <hana-da@users.noreply.github.com>
2020-05-12 15:41:24 +0900

    The production environment required BASIC authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :http_basic_authenticate_for_production
   before_action :devise_permitted_parameters, if: :devise_controller?
+
+  protected def http_basic_authenticate_for_production
+    return unless Rails.env.production?
+
+    http_basic_authenticate_or_request_with name:     ENV.fetch('BASIC_AUTH_USERNAME'),
+                                            password: ENV.fetch('BASIC_AUTH_PASSWORD')
+  end
 
   protected def devise_permitted_parameters
     devise_parameter_sanitizer.permit :sign_up, keys: %i[username profile blog_url]
---
+++ spec/requests/application_controllers_spec.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'ApplicationControllers', type: :request do
+  context 'RAILS_ENV が production の時' do
+    before do
+      allow(Rails.env).to receive(:production?).and_return(true)
+    end
+
+    context '環境変数: BASIC_AUTH_USERNAMEが存在しないと' do
+      before do
+        stub_const('ENV', ENV.to_h.tap { |h| h.delete('BASIC_AUTH_USERNAME') })
+      end
+
+      it do
+        expect { get root_path }.to raise_error(KeyError)
+      end
+    end
+
+    context '環境変数: BASIC_AUTH_PASSWORDが存在しないと' do
+      before do
+        stub_const('ENV', ENV.to_h.tap { |h| h.delete('BASIC_AUTH_PASSWORD') })
+      end
+
+      it do
+        expect { get root_path }.to raise_error(KeyError)
+      end
+    end
+
+    context '認証情報を環境変数に登録していれば' do
+      let(:username) { 'Someone' }
+      let(:password) { 'to Watch Over Me' }
+
+      before do
+        stub_const('ENV', ENV.to_h.merge('BASIC_AUTH_USERNAME' => username,
+                                         'BASIC_AUTH_PASSWORD' => password))
+      end
+
+      it 'Basic認証がかかっている(401 Unauthorized)' do
+        get '/'
+
+        expect(response).to have_http_status(:unauthorized)
+      end
+
+      it '認証情報が合致しない時も401 Unauthorized' do
+        get '/', headers: { 'HTTP_AUTHORIZATION' => "Basic #{Base64.strict_encode64('evil:attack')}" }
+
+        expect(response).to have_http_status(:unauthorized)
+      end
+
+      it '認証情報が合致すれば200 OKになる' do
+        get '/', headers: { 'HTTP_AUTHORIZATION' => "Basic #{Base64.strict_encode64("#{username}:#{password}")}" }
+
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+end
