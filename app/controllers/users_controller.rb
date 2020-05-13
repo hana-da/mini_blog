cb3da4eadfc79edd1bd046d92f7e9d68b097b326
hana-da <hana-da@users.noreply.github.com>
2020-05-13 22:17:13 +0900

    Put a follow button and an unfollow button on the user's profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,12 +9,12 @@ class UsersController < ApplicationController
 
   def follow
     current_user.follow(User.find(params[:id]))
-    redirect_to root_path
+    redirect_back fallback_location: root_path
   end
 
   def unfollow
     current_user.unfollow(User.find(params[:id]))
-    redirect_to root_path
+    redirect_back fallback_location: root_path
   end
 
   def timeline
--- spec/features/users/username_spec.rb
+++ spec/features/users/username_spec.rb
@@ -45,4 +45,64 @@ RSpec.describe '/users/:username', type: :feature do
 
     expect(page).to have_current_path(root_path)
   end
+
+  context 'ログインしていない時' do
+    it 'フォローする/フォロー解除のボタンは表示されてない' do
+      user = FactoryBot.create(:user)
+
+      visit user_path(user)
+
+      expect(page).not_to have_button(t('helpers.submit.follow'))
+      expect(page).not_to have_button(t('helpers.submit.unfollow'))
+    end
+  end
+
+  context 'ログインしている時' do
+    let!(:user) { FactoryBot.create(:user) }
+
+    before do
+      sign_in(user)
+    end
+
+    context 'フォローしているユーザのページだと' do
+      it 'フォロー解除ボタンが表示されていて、押すとフォロー解除できる' do
+        following = FactoryBot.create(:user).tap { |u| user.follow(u) }
+        expect(user).to be_following(following)
+
+        visit user_path(following)
+        expect(page).not_to have_button(t('helpers.submit.follow'))
+
+        click_button(t('helpers.submit.unfollow'))
+        user.reload
+
+        expect(user).not_to be_following(following)
+        expect(page).to have_current_path(user_path(following))
+      end
+    end
+
+    context 'フォローしていないユーザのページだと' do
+      it 'フォローするボタンが表示されていて、押すとフォローできる' do
+        not_following = FactoryBot.create(:user)
+        expect(user).not_to be_following(not_following)
+
+        visit user_path(not_following)
+        expect(page).not_to have_button(t('helpers.submit.unfollow'))
+
+        click_button(t('helpers.submit.follow'))
+        user.reload
+
+        expect(user).to be_following(not_following)
+        expect(page).to have_current_path(user_path(not_following))
+      end
+    end
+
+    context '自分自身のページだと' do
+      it 'フォローする/フォロー解除のボタンは表示されてない' do
+        visit user_path(user)
+
+        expect(page).not_to have_button(t('helpers.submit.follow'))
+        expect(page).not_to have_button(t('helpers.submit.unfollow'))
+      end
+    end
+  end
 end
