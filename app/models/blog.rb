d2c8c13b67083d0cf7a2edea6859a8a800b27223
hana-da <hana-da@users.noreply.github.com>
2020-06-15 14:17:53 +0900

    Consistent model association and validation order

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -22,15 +22,16 @@
 class Blog < ApplicationRecord
   after_validation :modify_errors_message_for_image
 
-  validates :content, length: { in: 1..140 }
-
   belongs_to :user
 
   has_many :comments, class_name: 'BlogComment', dependent: :destroy
   has_many :likes, class_name: 'UserFavoriteBlog', dependent: :destroy
+
   has_many :liked_users, through: :likes, source: :user
 
   mount_uploader :image, BlogImageUploader
+
+  validates :content, length: { in: 1..140 }
 
   scope :for_timeline, -> { order(created_at: :desc).preload(:user, :liked_users, comments: [:user]) }
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -23,26 +23,27 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, authentication_keys: [:username]
 
-  validates :username, uniqueness: true
-  validates :username, length: { in: 1..20 }
-  validates :username, format: { with: /\A[a-zA-Z]+\z/ }, allow_blank: true
+  has_many :blog_comments, dependent: :destroy
+  has_many :blogs, dependent: :destroy
+  has_many :follower_relationships, class_name: 'UserRelationship', foreign_key: :followed_id,
+                                    inverse_of: :followed, dependent: :destroy
+  has_many :following_relationships, class_name: 'UserRelationship', foreign_key: :follower_id,
+                                     inverse_of: :follower, dependent: :destroy
+  has_many :likes, class_name: 'UserFavoriteBlog', dependent: :destroy
+
+  has_many :followers, through: :follower_relationships,  source: :follower
+  has_many :following, through: :following_relationships, source: :followed
+  has_many :likes_blogs, through: :likes, source: :blog
+
+  validates :email, format: { with: /\A.+@.+\..+\z/ }, allow_blank: true
   validates :email, presence: true
   validates :email, uniqueness: { case_sensitive: false }, if: ->(u) { u.email.present? }
-  validates :email, format: { with: /\A.+@.+\..+\z/ }, allow_blank: true
   validates :password, confirmation: true
   validates :password, presence: true, on: :create
   validates :profile, length: { maximum: 200 }
-
-  has_many :blogs, dependent: :destroy
-  has_many :blog_comments, dependent: :destroy
-  has_many :following_relationships, class_name: 'UserRelationship',
-                                     foreign_key: :follower_id, inverse_of: :follower, dependent: :destroy
-  has_many :follower_relationships,  class_name: 'UserRelationship',
-                                     foreign_key: :followed_id, inverse_of: :followed, dependent: :destroy
-  has_many :followers, through: :follower_relationships,  source: :follower
-  has_many :following, through: :following_relationships, source: :followed
-  has_many :likes, class_name: 'UserFavoriteBlog', dependent: :destroy
-  has_many :likes_blogs, through: :likes, source: :blog
+  validates :username, format: { with: /\A[a-zA-Z]+\z/ }, allow_blank: true
+  validates :username, length: { in: 1..20 }
+  validates :username, uniqueness: true
 
   # @return [String]
   def to_param
